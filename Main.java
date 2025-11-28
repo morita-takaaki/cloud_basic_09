7f001a3ea6ba5eb52530c4629816995ccdb0b046
MORITA Takaaki <morita@chuo-ai.jp>
2025-11-28 23:14:15 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 import java.sql.*;
-
 public class Main {
     public static void main(String[] args) {
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:student.db")) {
+        try {
+            // SQLite DB に接続
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:student.db");
             Statement stmt = conn.createStatement();
+
+            // DB の内容を取得
             ResultSet rs = stmt.executeQuery("SELECT * FROM students");
 
             while (rs.next()) {
@@ -13,6 +16,8 @@ public class Main {
                     rs.getString("email")
                 );
             }
+
+            conn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
